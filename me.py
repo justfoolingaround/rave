c79834c1aa9603f2d4c31ff5929189271c742f5b
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-06-04 22:56:11 +0545

    Chore: Use `upstream` gitcord

--- me.py
+++ me.py
@@ -12,10 +12,11 @@ bot = RaveBot(
 )
 
 bot.load_extension("jishaku")
+bot.load_extension("gitcord")
+
 bot.load_extension("rave.cogs.client.events")
 bot.load_extension("rave.cogs.client.base")
 bot.load_extension("rave.cogs.sauce")
-bot.load_extension("rave.cogs.gitcord")
 bot.load_extension("rave.cogs.waiifubot")
 bot.load_extension("rave.cogs.dinkies")
 
